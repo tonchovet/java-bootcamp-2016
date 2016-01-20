b4b8d5759b5a93068fc89b5222eef2d09885ca12
alberto ghibaudo <alberto ghibaudo>
2016-01-20 02:24:09 -0300

    Topic 2.2

---
+++ Topic 2.2/Topic/src/main/java/alber/Topic/App.java
@@ -0,0 +1,76 @@
+package alber.Topic;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static TreeMap < Integer, Message > blog = Blog.getInstance();
+	
+	public ArrayList<String> getAuthorMessage( String author )
+		{
+			Message message = new Message();
+		
+			ArrayList<String> authorMessage = new ArrayList<String>();
+		
+			// Get a set of the entries
+			Set set = Blog.getInstance().entrySet();
+			// Get an iterator
+			Iterator i = set.iterator();
+			// Display elements
+			while(i.hasNext()) {
+	    	  
+				Map.Entry me = (Map.Entry)i.next();
+	         
+				message = blog.get(me.getKey());
+				
+				if ( message.getAuthor() == author )
+				{
+					System.out.println(me.getKey() + ": " + message.getMessage());
+				}
+			}
+		
+		return authorMessage;
+		}
+	
+    public static void main( String[] args )
+    {
+    	
+    	Message message1 = new Message();
+    	
+    	message1.setAuthor("alber");
+    	
+    	message1.setMessage("algo pusieron primero");
+    	
+    	blog.put(1, message1);
+    	
+    	Message message2 = new Message();
+    	
+    	message2.setAuthor("fabricio");
+    	
+    	message2.setMessage("algo pusieron despues");
+    	
+    	blog.put(2, message2);
+    	
+    	Message message3 = new Message();
+    	
+    	message3.setAuthor("alber");
+    	
+    	message3.setMessage("algo pusieron despues de eso");
+    	
+    	blog.put(3, message3);
+    	
+        System.out.println( "Hello World!" );
+        
+        Blog.getAuthorMessage("fabricio");
+        
+    }
+    
+}
