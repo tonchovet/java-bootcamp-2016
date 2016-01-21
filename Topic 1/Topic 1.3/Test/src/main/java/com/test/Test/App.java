d3595d6fb72719240de314bff6f46c3f3d28f8ff
alberto ghibaudo <alberto ghibaudo>
2016-01-21 03:38:47 -0300

    Topic 1

---
+++ Topic 1/Topic 1.3/Test/src/main/java/com/test/Test/App.java
@@ -0,0 +1,25 @@
+package com.test.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public int CalcularSuma()
+	{
+		int num1 = 3, num2 = 2;
+		
+		int result = 0;
+		
+		return result = num1 + num2;
+	}
+	
+    public static void main( String[] args )
+    {
+    	
+    	App app = new App();
+    	
+         System.out.println( "" + app.CalcularSuma() + "" );
+    }
+}
